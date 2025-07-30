61e7f0472d3c7e9e1deae68bdc403d69eba7af67
MouMou <130217789+MouMou-Eleven@users.noreply.github.com>
2025-07-31 03:13:26 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,19 +18,19 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '超级个体',
+  HEO_HERO_TITLE_2: '全栈AI赋能',
+  HEO_HERO_TITLE_3: '一名设计师的AI学习之路',
+  HEO_HERO_TITLE_4: '网站推荐',
+  HEO_HERO_TITLE_5: '十一创导航网站欢迎您',
+  HEO_HERO_TITLE_LINK: 'https://moumou-eleven.github.io/yang-hub/yang-hub/index.html',
   // 英雄区遮罩文字
-  HEO_HERO_COVER_TITLE: '随便逛逛',
+  HEO_HERO_COVER_TITLE: '随机观文',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '文章收录', url: '/tag/文章收录' },
+  HEO_HERO_CATEGORY_2: { title: 'AI内容分享', url: '/tag/AI内容分享' },
+  HEO_HERO_CATEGORY_3: { title: '工作待办', url: '/tag/工作待办' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -39,7 +39,7 @@ const CONFIG = {
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
-    '你好！我是',
+    '你好！我是杨建委YJW',
     '🔍 分享与热心帮助',
     '🤝 专修交互与设计',
     '🏃 脚踏实地行动派',
@@ -51,7 +51,7 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
+  HEO_INFO_CARD_URL2: 'https://www.xiaohongshu.com/user/profile/671a0d14000000001d021f4e?xsec_token=AB-U_1qRELuZc-KMWzWLiRXGMX5LqsAkVaOTACXliQjsc%3D&xsec_source=pc_search',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
   HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
   HEO_INFO_CARD_TEXT3: '了解更多',
